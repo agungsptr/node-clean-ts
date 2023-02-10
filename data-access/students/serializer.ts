8d7ec521dd63942afd69bf732ae3a893c10d78bf
agungsptr <agung.e.sptr@gmail.com>
2023-02-10 21:38:06 +0800

    main: fix serialize issue when receive null

--- data-access/students/serializer.ts
+++ data-access/students/serializer.ts
@@ -1,15 +1,15 @@
 import { serializer } from "../../commons/utils";
 
-function serializeSingle(data: Record<string, any>) {
+function serializeSingle(data?: Record<string, any>) {
   return {
-    id: data._id,
-    grade: data.grade,
-    name: data.name,
-    age: data.age,
-    perfect: data.perfect,
-    createdBy: data.createdBy,
-    createdAt: data.createdAt,
-    updatedAt: data.updatedAt,
+    id: data?._id,
+    grade: data?.grade,
+    name: data?.name,
+    age: data?.age,
+    perfect: data?.perfect,
+    createdBy: data?.createdBy,
+    createdAt: data?.createdAt,
+    updatedAt: data?.updatedAt,
   };
 }
 
--- data-access/users/serializer.ts
+++ data-access/users/serializer.ts
@@ -1,13 +1,13 @@
 import { serializer } from "../../commons/utils";
 
-function serializeSingle(data: Record<string, any>) {
+function serializeSingle(data?: Record<string, any>) {
   return {
-    id: data._id,
-    firstName: data.firstName,
-    lastName: data.lastName,
-    username: data.username,
-    createdAt: data.createdAt,
-    updatedAt: data.updatedAt,
+    id: data?._id,
+    firstName: data?.firstName,
+    lastName: data?.lastName,
+    username: data?.username,
+    createdAt: data?.createdAt,
+    updatedAt: data?.updatedAt,
   };
 }
 
